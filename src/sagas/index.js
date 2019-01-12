509b91d06c16757c4141a785607499ebe40e6748
Steven Gong <steven.gong@iag.com.au>
2019-01-12 13:00:10 +1100

    remove app saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,5 @@
 import { all, fork } from 'redux-saga/effects';
 
-import app from './appxxxxx';
 import github from './github';
 // import user from './user';
 
@@ -8,5 +7,5 @@ import github from './github';
  * rootSaga
  */
 export default function* root() {
-  yield all([fork(app), fork(github)]);
+  yield all([fork(github)]);
 }
